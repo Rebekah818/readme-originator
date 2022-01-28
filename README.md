67b34c6f29f7d92186ed37a542a5f44b643e0cc2
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-01-28 14:55:46 -0500

    added license badge and link to generateMarkdown

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# readme-originator
+# asdf
+# asdf 
+# asdf
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./generateMarkdown');
-
+const x = {
+    projectName: "project name"
+}
 // TODO: Create an array of questions for user input
 const promptQuestions = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'project name',
+            name: 'projectname',
             message: 'What is the name of your project? (required)',
             validate: projectName => {
                 if (projectName) {
@@ -45,24 +47,41 @@ const promptQuestions = () => {
                 }
             }
         },
-
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license',
+            choices: ['MIT','Boost','Apache','no license'],
+            // // validate: (licenseInput) => {
+            // //     if (licenseInput) {
+            // //         return true;
+            // //     } else {
+            // //         console.log('Please select from the license list');
+            // //         return false;
+            // //     }
+            // }
+        }
     ]);
 };
 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const writeToFile = generateMarkdown(readMeData);
-    fs.writeFile('./index.html', writeToFile, err => {
+    
+    fs.writeFile(fileName, data, err => {
         if (err) throw new Error(err);
-        console.log('README created! Check out index.html to view it!');
+        console.log('README created! Check out README.md to view it!');
     });
-}
- 
+};
 
 // TODO: Create a function to initialize app
-function init() { }
+async function init() {
+    const response= await promptQuestions();
+    console.log(response);
+ const markdown = await generateMarkdown(response);
+ writeToFile('README.md', markdown);
+};
 
 // Function call to initialize app
-promptQuestions()
-.then()
+init()
+
